89adc39e53a8717557ada8b738a649099ced868a
Ugonz86 <ugonzalez86@gmail.com>
2022-05-06 16:06:00 -0700

    Updates

--- src/redux/actions/index.js
+++
@@ -1,5 +0,0 @@
-import { FETCHDATA } from "../constants/action-types";
-
-export function fetchData() {
-    return { type: FETCHDATA, payload: {} };
-}
--- src/redux/constants/action-types.js
+++
@@ -1,1 +0,0 @@
-export const FETCHDATA = "FETCHDATA";
