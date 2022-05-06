4b85dffb5a35a9d7c61b2c5b8bc713b935aced5a
Ugonz86 <ugonzalez86@gmail.com>
2022-05-06 16:38:06 -0700

    Modify index

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from "./redux/store";
+import './index.css';
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-render(
+ReactDOM.render(
   <Provider store={store} >
     <App />
   </Provider>,
