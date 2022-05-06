0e3f37048d3f58ed1a32cb6c5b34745781eeb8cc
Ugonz86 <ugonzalez86@gmail.com>
2022-05-06 16:30:39 -0700

    Ready for deployment

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// render(
-//   <Provider store={store} >
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
 //     <App />
-//   </Provider>,
-//   document.getElementById("root")
+//   </React.StrictMode>
 // );
+
+render(
+  <Provider store={store} >
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
